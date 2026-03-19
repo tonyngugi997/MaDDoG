22dc144fa0b840348dd80f26ca6ded0738f3cd39
tonyngugi997 <tonyngugi997@gmail.com>
2026-03-19 10:17:54 +0300

    feat::Reading from the TcpStream and printing the data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{BufReader, prelude::*},   
+};
 
 fn main () {
     let listener = TcpListener::bind("0.0.0.0:5000").unwrap();
@@ -6,13 +9,18 @@ fn main () {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("connection established");
+        handle_connection(stream);
+
     }
 
+    fn handle_connection(mut stream: TcpStream) {
+        let buf_reader = BufReader::new(&stream);
+        let http_request: Vec<_> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+        println!("htt_request: {:#?}", http_request);  
 
-
-
-
-
-
+    }
 }
