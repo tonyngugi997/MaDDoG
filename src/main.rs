746fa0be7c6657dc663c5bd4c81ddcb6785a711b
tonyngugi997 <tonyngugi997@gmail.com>
2026-03-19 10:10:43 +0300

    Listing 21-3: Writing a tiny successful HTTP response to the stream

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,24 @@ use std::{
 };
 
 fn main () {
-    let listener = TcpListener::bind("0.0.0.0:5000").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:5000").unwrap();
     println!("server listening on port 5000");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
-
     }
 
-    fn handle_connection(mut stream: TcpStream) {
-        let buf_reader = BufReader::new(&stream);
-        let http_request: Vec<_> = buf_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
-        println!("htt_request: {:#?}", http_request);  
+}
 
-    }
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
+    stream.write_all(response.as_bytes()).unwrap();
 }
